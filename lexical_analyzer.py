5a5dfd772431072c336091c9f8fc88a851660acf
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-09-22 23:54:48 +0500

    lexical analyzer

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -6,23 +6,27 @@ token_specification = [
     ("END",      r';'),             # Statement terminator
     ("ID",       r'[A-Za-z_]\w*'),  # Identifiers
     ("OP",       r'[+\-*/]'),       # Arithmetic operators
+    # Comparison operators (>, <, >=, <=, ==, !=)
+    ("COMPARE",  r'[<>]=?|==|!='),
     ("NEWLINE",  r'\n'),            # Line endings
     ("SKIP",     r'[ \t]+'),        # Skip spaces and tabs
     ("MISMATCH", r'.'),             # Any other character
 ]
 
 
-tok_regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in token_specification)
+tok_regex = '|'.join(f'(?P<{name}>{pattern})' for name,
+                     pattern in token_specification)
 get_token = re.compile(tok_regex).match
+
 
 def lexical_analyzer(code):
     line_num = 1
     pos = line_start = 0
     tokens = []
     mo = get_token(code)
-    
+
     while mo:
-        kind = mo.lastgroup
+        kind = mo.lastgroup  # token type
         value = mo.group()
         if kind == "NUMBER":
             value = float(value) if '.' in value else int(value)
@@ -37,16 +41,19 @@ def lexical_analyzer(code):
         elif kind == "SKIP":
             pass
         elif kind == "MISMATCH":
-            raise RuntimeError(f"Unexpected character {value!r} on line {line_num}")
+            raise RuntimeError(
+                f"Unexpected character {value!r} on line {line_num}")
         else:
             tokens.append((kind, value))
-        
+
         pos = mo.end()
         mo = get_token(code, pos)
-    
+
     return tokens
 
-code = """x = 10;
+
+code = """
+x = 10;
 y = x + 5;
 if y > 10
     y = y - 1;
